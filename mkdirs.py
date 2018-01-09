485a431e7c9e5b4fa06db43edbbe921d98c0a63b
Harsh Thaker <hst.sidhpur@gmail.com>
2018-01-09 16:27:30 +0100

    function commit.

---
+++ mkdirs.py
@@ -0,0 +1,7 @@
+list_array = [1,2,3,4]
+
+def create_dirs(path):
+    for dir_name in unique_labels:
+        output_path = os.path.join(path, dir_name)
+        if not os.path.exists(output_path):
+            os.makedirs(output_path)
